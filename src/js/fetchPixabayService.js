3e7dd6dd4b921f3fa2a2c68e3702bc6307fc4547
Олександр Канюка <137859612+crusaider423@users.noreply.github.com>
2023-11-27 20:10:58 +0200

    update

--- src/js/fetchPixabayService.js
+++ src/js/fetchPixabayService.js
@@ -18,7 +18,7 @@ export async function fetchService(value, page) {
 
   // const { data } = await axios.get(BASE_URL, options);
   // return data;
-  return axios.get(`https://pixabay.com/api/?key=40831158-570978b10e9a43fe0d856b395&q=${value}&image_type=photo&orientation=horizontal&safesearc=true&page=${page}&per_page=40`).then(({ data }) => data);
+  return axios.get(`${BASE_URL}/?key=${API_KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearc=true&page=${page}&per_page=40`).then(({ data }) => data);
 }
 
 // return axios.get(BASE_URL, options).then(({ data }) => data);
